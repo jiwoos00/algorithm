935220f1f8fb8742ccb015db304026e0be338186
jiwoos1118 <101661684+jiwoos1118@users.noreply.github.com>
2023-10-09 20:27:01 +0900

    update

--- baekjoon/11403.py
+++ baekjoon/11403.py
@@ -11,43 +11,8 @@ for k in range(N):
         for b in range(N):
             if graph[a][b] == 1:
                 continue
-            if graph[a][k] and graph[k][b] :
+            if graph[a][k] and graph[k][b]:
                 ans[a][b] = 1
           
 for i in range(N):
     print(' '.join(map(str, ans[i])))
-
-# INF = int(1e9)
-
-# # 노드 개수 및 간선 개수
-# n = int(input())
-# m = int(input())
-
-# # 2차원 리스트 초기화
-# graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
-# # 자기 자신으로 가는 비용은 0으로 초기화
-# for a in range(1, n + 1):
-#     for b in range(1, n + 1):
-#         if a == b:
-#             graph[a][b] = 0
-
-# # 각 간선에 대한 정보 초기화
-# for _ in range(m):
-#     a, b, c = map(int, input().split())
-#     graph[a][b] = c
-
-# # 점화식에 따른 알고리즘 수행
-# for k in range(1, n + 1):
-#     for a in range(1, n + 1):
-#         for b in range(1, n + 1):
-#             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
-# # 수행 결과 출력
-# for a in range(1, n + 1):
-#     for b in range(1, n + 1):
-#         if graph[a][b] == INF:
-#             print("INFINITY", end = ' ')
-#         else:
-#             print(graph[a][b], end = ' ')
-#     print()
--- baekjoon/1389.py
+++ baekjoon/1389.py
@@ -3,6 +3,9 @@ N, M = map(int, input().split())
 INF = float('inf')
 friend = [[INF] * N for _ in range(N)]
 
+for i in range(N):
+    friend[i][i] = 0
+
 for _ in range(M):
     i, j = map(int, input().split())
     friend[i - 1][j - 1] = 1
@@ -11,11 +14,8 @@ for _ in range(M):
 for k in range(N):
     for i in range(N):
         for j in range(N):
-                if friend[i][j] > friend[i][k] + friend[k][j]:
-                    friend[i][j] = friend[i][k] + friend[k][j]
+            friend[i][j] = min(friend[i][j], friend[i][k] + friend[k][j])
 
-
-print(friend)
 
 ans = []
 for i in range(N):
