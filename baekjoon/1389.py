c3ffa8acf48ab33c3e6c92a7836ff007208949cc
jiwoos1118 <101661684+jiwoos1118@users.noreply.github.com>
2023-10-09 20:11:15 +0900

    update

---
+++ baekjoon/1389.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+
+INF = float('inf')
+friend = [[INF] * N for _ in range(N)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    friend[i - 1][j - 1] = 1
+    friend[j - 1][i - 1] = 1
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+                if friend[i][j] > friend[i][k] + friend[k][j]:
+                    friend[i][j] = friend[i][k] + friend[k][j]
+
+
+print(friend)
+
+ans = []
+for i in range(N):
+    ans.append(sum(friend[i]))
+m = min(ans)
+
+print(ans.index(m) + 1)
+
